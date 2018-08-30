cfbb44a26c27605815c98ee9f63e23332d278222
Yacine Sibous <yacinesibous@gmail.com>
2018-08-29 17:09:57 -0700

    added

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,12 +1,13 @@
 defmodule Dictionary do
-  def random_word(word_list) do
-    word_list
+  def random_word() do
+    word_list()
     |> Enum.random
   end
 
   def word_list do
     "../assets/words.txt"
-    |> File.read!
+    |> Path.expand(__DIR__)
+    |> File.read!()
     |> String.split(~r/\n/)
   end
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -1,8 +1,37 @@
 defmodule HangmanTest do
   use ExUnit.Case
-  doctest Hangman
 
-  test "greets the world" do
-    assert Hangman.hello() == :world
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+    assert game.letters |> Enum.map(fn letter -> letter == String.downcase(letter) end)
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occcurence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occcurence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
 end
